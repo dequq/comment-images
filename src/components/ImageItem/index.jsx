5670029913ce558882e556ca79935d8c1d4f0b1f
azhidkov <dafg.dio@gmail.com>
2022-09-05 17:52:40 +0300

    commit

--- src/components/ImageItem/index.jsx
+++ src/components/ImageItem/index.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import "./image.scss";
 
 const ImageItem = ({ images, handleOpenModal }) => {
   return (
     <>
       {images?.map((item) => {
         return (
-          <div key={item.id} onClick={() => handleOpenModal(item.id)}>
-            <img src={item.url} alt={item.id} />
+          <div
+            className="img"
+            key={item.id}
+            onClick={() => handleOpenModal(item.id)}
+          >
+            <img className="img__block" src={item.url} alt={item.id} />
           </div>
         );
       })}
